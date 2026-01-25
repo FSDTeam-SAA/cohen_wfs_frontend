820d825d443591ffa165547d5a639021a64babd9
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-26 05:27:13 +0600

    fix: nav and footer visibility

--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
+import Navbar from "@/components/shared/navbar";
+import Footer from "@/components/shared/footer";
+import { usePathname } from "next/navigation";
 
 export default function AppProvider({
   children,
@@ -12,10 +15,17 @@ export default function AppProvider({
 }) {
   const queryClient = new QueryClient();
 
+  const pathname = usePathname();
+
+  const isHidNavFooter = pathname.startsWith("/auth/");
+
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
-        {children} <Toaster position="top-right" />
+        {!isHidNavFooter && <Navbar />}
+        {children}
+        {!isHidNavFooter && <Footer />}
+        <Toaster position="top-right" />
       </QueryClientProvider>
     </SessionProvider>
   );
